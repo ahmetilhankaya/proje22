8a6f09eceacb9dd39dc6abe8ab54b3e1f590b96a
akkayagokhan <032180094@ogr.uludag.edu.tr>
2022-12-05 21:43:10 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -8,9 +8,11 @@ BLACK = (  0,   0,   0)
 FPS = 15
 
 pygame.init()
+
 # pencere büyüklüğü, bilgisayarda oluşturulan pencerelerde
 # koordinatlar sol üstten başlar, sağ alta doğru büyür
 # sol üstteki koordinat noktası (0,0)'dır. 
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 #pencerenin adı
 pygame.display.set_caption("PyGame Direnc Hesaplama")
