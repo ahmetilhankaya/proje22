1323ef8f685054e209255915a55ea8a012f6c081
akkayagokhan <032180094@ogr.uludag.edu.tr>
2022-12-05 21:45:18 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -71,19 +71,22 @@ rho_rect.centerx = rho_scroll_bar.centerx
 # bu fonksiyon kırmızı kutunun, arkaplanındaki siyah çubuğun tepesine olan uzaklığını hesaplayarak 0.01 ile çarpıyor
 # ve round() fonksiyonunu kullanarak bu float türündeki sayının 2 decimal basamağını hesaplıyor 
 # örneğin round(0.12345, 2) fonksiyonu 0.12 döndürür, kalan basamakları keser.
+
 def resistivity():
     return round((rho_scroll_bar_y + rho_scroll_bar_height - rho_scroll_box.y - (rho_scroll_box.height / 2) + 1) * 0.01,2)
+
 # kırmızı kutucuk(rho_scroll_box) hareket ettikçe, öz direnç değeri yeniden hesaplanacak ve metin güncellenecek
+
 rho_value_font = font.render(f"%.2f" %resistivity(), True, pygame.Color(0,0,0,255)) 
 rho_value_rect = rho_value_font.get_rect()
 rho_value_rect.top = rho_scroll_bar_y + rho_scroll_bar_height + 10
 rho_value_rect.centerx = rho_scroll_bar_x + int(rho_scroll_bar_width / 2)
 
 
-#
+
 # uzunluk değeri için gerekli scrollbar ve scrollbox'un koordinatları, ve büyüklükleri
 #   öz direnç için kullanılan bar ve box ile aynı özelliklere sahip
-# 
+
 len_scroll_bar_x,len_scroll_bar_y = 250, 100
 len_scroll_bar_width, len_scroll_bar_height  = 2, 95
 len_scroll_box_width, len_scroll_box_height = 20, 10
