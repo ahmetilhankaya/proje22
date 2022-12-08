3997e0a17c583ff2eb93f9520e3ea4e2b3d2bf71
levent3435 <96023351+levent3435@users.noreply.github.com>
2022-12-08 23:33:48 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -164,5 +164,5 @@ def area():
     return round(min + x,2)
 area_value_font = font.render(f"%.2f" %area(), True, pygame.color(0,0,0,255))
 area_value_rect = area_value_font.get_rect()
-
+area_value_rect.top = area_scroll_bar_y + area_scroll_bar_height + 10
 
