73a02deed4622360b6e9215decda6a259fc82e86
ahmetilhankaya <032180096@ogr.uludag.edu.tr>
2022-12-09 22:34:43 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -183,4 +183,5 @@ def resistance():
 
 reistance_font = pygame.font.Font(pygame.font.get_default_font(), int(resistance()) + 10 )
 division_r = reistance_font.render('R', False, pygame.Color(12,58,160,255))
-division_r = division_r.get_rect()
+division_r_text = division_r.get_rect()
+division_r_text.centery = area_scroll_bar_y + 70
