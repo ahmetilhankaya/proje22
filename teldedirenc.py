d7f25ad6e6ddd5df23138d6d34b4259235e117e9
ahmetilhankaya <032180096@ogr.uludag.edu.tr>
2022-12-17 21:51:17 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -287,3 +287,4 @@ def update_area_value_text(_area):
     area_value_rect = area_value_font.get_rect()
     area_value_rect.top = area_scroll_bar_y + area_scroll_bar_height + 10
     area_value_rect.centerx = area_scroll_bar_x + int(area_scroll_bar_width / 2)
+    cable_area_y = int(_area * 5) + 5
