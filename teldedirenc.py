d2662620e08e092c8d47a318603add739dffae16
levent3435 <96023351+levent3435@users.noreply.github.com>
2022-12-18 16:31:00 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -300,7 +300,9 @@ def update_division_area_size(_area):
     division_area_text.centery = area_scroll_bar_y + 160
     division_area_text.centerx = area_scroll_bar_x + 390
     
-print(rho_scroll_box.y = 145, len_scroll_box.y, area_scroll_box.y)    
+# kırmızı kutuların ilk koordinatlarını göremke için:    
+print(rho_scroll_box.y = 145, len_scroll_box.y, area_scroll_box.y) 
+
     
 # oyunun ana fonksiyonu
 def run_game():
