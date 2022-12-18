35df5c8b1fd96ecd6d1ffc42750dcf1b0333f0ff
levent3435 <96023351+levent3435@users.noreply.github.com>
2022-12-18 17:10:17 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -306,4 +306,5 @@ def update_division_area_size(_area):
     
 # oyunun ana fonksiyonu
 def run_game():
+    # global değişkenleri fonksiyonun içinde kullanmak için tanımlama yapıyoruz
     global rho_scroll_box_dragging , len_scroll_box_dragging , area_scroll_box_dragging , division_r , division_r_text , calculated_resistance , calculated_resistance_text , cable_color , cable_length
