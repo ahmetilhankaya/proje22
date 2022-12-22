13e30804851ac1e9d67ccc90df72039cda58777b
muhammed semih asilkan <032080051@ogr.uludag.edu.tr>
2022-12-23 00:01:38 +0300

    1

--- teldedirenc.py
+++ teldedirenc.py
@@ -396,6 +396,7 @@ def run_game():
                 update_len_value_text(_length)
                 update_division_l_size(_length)
                 update_rho_value_text(_resistivity)
+                
 
 
 
