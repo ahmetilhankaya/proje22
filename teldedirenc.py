945858f62a0e5fecda5440916e0fcf7ecbea8ce4
levent3435 <96023351+levent3435@users.noreply.github.com>
2022-12-27 23:49:11 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -460,6 +460,7 @@ def run_game():
         
         ellipse_start = pygame.Rect(cable_start_x, cable_start_y, cable_area_x, cable_area_y)
         pygame.draw.ellipse(screen, cable_color, ellipse_start)
+        pygame.draw.rect(screen, cable_color, (ellipse_start.centerx, ellipse_start.y, cable_length * 2 + 10, cable_area_y))
 
 
 
