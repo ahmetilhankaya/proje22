2886f80537f9183a8dfa45a128bbd5de8e8a78d7
levent3435 <96023351+levent3435@users.noreply.github.com>
2022-12-28 00:03:14 +0300

    Update teldedirenc.py

--- teldedirenc.py
+++ teldedirenc.py
@@ -460,6 +460,7 @@ def run_game():
         
         # kabloyu çizmek
         # kablo için 2 elips 1 dikdörtgen kullandık
+        # dikdörtgeninin başı ve sonu elipslerin tam ortasından geçecek
         ellipse_start = pygame.Rect(cable_start_x, cable_start_y, cable_area_x, cable_area_y)
         pygame.draw.ellipse(screen, cable_color, ellipse_start)
         pygame.draw.rect(screen, cable_color, (ellipse_start.centerx, ellipse_start.y, cable_length * 2 + 10, cable_area_y))
